2c94f6cd01a668e107f956120925acb1cdc5fad2
ttb11 <31479156+ttb11@users.noreply.github.com>
2017-12-07 21:33:13 -0500

    upload javascript

---
+++ website/html/Untitled1.js
@@ -0,0 +1,122 @@
+function myFunction() {
+var x = document.getElementById("thirdThird")
+if (x.style.display == 'none') {
+    x.style.display='block';
+    } else {
+        x.style.display='none';
+    }
+ }
+function toggle() {
+    var x = document.getElementById("firstFirst");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showhide() {
+    var x = document.getElementById("secondSecond");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggle4() {
+    var x = document.getElementById("fourthFourth");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggle5() {
+    var x = document.getElementById("fifthFifth");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggle6() {
+    var x = document.getElementById("sixthSixth");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggle7() {
+    var x = document.getElementById("sevenSeventh");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggle8() {
+    var x = document.getElementById("eighthEighth");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggle9() {
+    var x = document.getElementById("ninthNinth");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggle0() {
+    var x = document.getElementById("tenTenth");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function contraction()
+{
+var chars = document.getElementsByClassName ("contraction")
+if (chars[0].style.color != "#FF3300")
+{
+for (var i = 0, length = chars.length; i < length; i++) {
+            chars[i].style.color = "#FF3300";
+}
+}
+else {
+  for (var i = 0, length = chars.length; i < length; i++) 
+{chars[i].style.color = "";}  
+}
+}
+function textese()
+{
+var chars = document.getElementsByClassName ("textese")
+if (chars[0].style.color != "blue")
+{
+for (var i = 0, length = chars.length; i < length; i++) {
+            chars[i].style.color = "blue";
+}
+}
+else {
+  for (var i = 0, length = chars.length; i < length; i++) 
+{chars[i].style.color = "";}  
+}
+}
+function informal()
+{
+var chars = document.getElementsByClassName ("informal")
+if (chars[0].style.color != "magenta")
+{
+for (var i = 0, length = chars.length; i < length; i++) {
+            chars[i].style.color = "magenta";
+}
+}
+else {
+  for (var i = 0, length = chars.length; i < length; i++) 
+{chars[i].style.color = "";}  
+}
+}
